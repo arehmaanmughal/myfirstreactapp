eab927d90b3afebf63e11199246344f4f744dfea
Abdulrehman <arehmaanmughal@gmail.com>
2024-03-13 05:51:49 +0500

    login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,53 +2,39 @@ import React, { useContext, useState } from "react";
 import UserContext from "../context/UserContext";
 import { useNavigate } from "react-router-dom";
 import { Button, Card } from "react-bootstrap";
-
+import "./Login.css";
 
 const Login = () => {
-  // const [username, setusername] = useState(null);
-  // const [password, setPassword] = useState(null);
   const { user, setUserid } = useContext(UserContext);
-  const navigate = useNavigate()
-  
+  const navigate = useNavigate();
+
   console.log(user);
   return (
     <div>
-      <h1>Users</h1>
       <div className="container">
-        <div className="row">
+        <h1>Users</h1>
+        <div className="row gy-4">
           {user?.map((users, index) => (
-            <div className="col" key={index}>
-              <Card style={{ width: "18rem" }}>
+            <div className="col " key={index}>
+              <Card
+                style={{ width: "16rem" }}
+                onClick={() => {
+                  setUserid(users?.id);
+                  navigate("/Profile");
+                }}
+              >
                 <Card.Body>
                   <Card.Title>{users?.name}</Card.Title>
-                  <Card.Subtitle className="mb-2 text-muted">{users?.username}</Card.Subtitle>
-                  <Card.Text>
-                    Some quick example text to build on the card title and make
-                    up the bulk of the card's content.
-                  </Card.Text>
-                  <Button onClick={()=> {
-                    setUserid(users?.id);
-                    navigate("/Profile")
-                  }} variant="primary">View Profile</Button>
+                  <Card.Subtitle className="mb-2 text-muted">
+                    {users?.username}
+                  </Card.Subtitle>
+                  <Card.Text></Card.Text>
                 </Card.Body>
               </Card>
             </div>
           ))}
         </div>
       </div>
-      {/* <input
-        type="text"
-        placeholder="username"
-        value={username}
-        onChange={(e) => setusername(e.target.value)}
-      />
-      <input
-        type="text"
-        placeholder="password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <button onClick={hanndleSubmit}>Submit</button> */}
     </div>
   );
 };
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,7 +1,7 @@
 import React, { useContext, useEffect, useState } from "react";
 import UserContext from "../context/UserContext";
 import { Card } from "react-bootstrap";
-
+// import "./Profile.css"
 const Profile = () => {
   const { userid } = useContext(UserContext);
   const [user, setUser] = useState([]);
@@ -15,16 +15,13 @@ const Profile = () => {
   if (!user) return <div>User Not found</div>;
   return (
     <div>
-      <Card style={{ width: "18rem" }}>
+      <Card style={{ width: "26rem" }} className="p-card">
         <Card.Body>
           <Card.Title>{user.name}</Card.Title>
           <Card.Subtitle className="mb-2 text-muted">
             {user.username}
           </Card.Subtitle>
-          <Card.Text>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </Card.Text>
+          <Card.Text></Card.Text>
         </Card.Body>
       </Card>
     </div>
